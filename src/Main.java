234351bd4d9ef521033fa90b2ff83a9549d2337b
Hovhannes Grigoryan <grhovo199811@gmail.com>
2021-03-13 19:34:00 +0400

    First commit, where we added ip checker, subnetmask checker, and converter from /x to x.x.x.x

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ipaddressoctets = new int[4];
+        int mask;
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < 4; i++) {
+            System.out.println("Give ip address of octet " + (i + 1));
+            ipaddressoctets[i] = scanner.nextInt();
+            while (ipaddressoctets[i] > 255 || ipaddressoctets[i] < 0) {
+               System.out.println("Please give correct number for octet");
+               ipaddressoctets[i] = scanner.nextInt();
+            }
+        }
+        System.out.println("Give subnet mask");
+        mask = scanner.nextInt();
+        while (mask > 32 || mask < 0){
+            System.out.println("Please give correct number for subnet mask");
+            mask = scanner.nextInt();
+        }
+        int[] subnetOctet = convertMaskToOctets(mask);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(subnetOctet[i]);
+        }
+
+    }
+
+    public static int[] convertMaskToOctets(int mask){
+        int maskChangedOctets = mask % 8;
+        int fullQualifiedOctets = mask/8;
+        int changedOctet;
+        int[] subnetOctet = new int[4];
+        changedOctet = switch (maskChangedOctets) {
+            case 0 -> 0;
+            case 1 -> 128;
+            case 2 -> 192;
+            case 3 -> 224;
+            case 4 -> 240;
+            case 5 -> 248;
+            case 6 -> 252;
+            case 7 -> 254;
+            default -> throw new IllegalStateException("Unexpected value: " + maskChangedOctets);
+        };
+        for(int i=0; i<4; i++){
+            if(i<fullQualifiedOctets){
+                subnetOctet[i] = 255;
+            }
+            else if(i==fullQualifiedOctets){
+                subnetOctet[i] = changedOctet;
+            }
+            else {
+                subnetOctet[i] = 0;
+            }
+        }
+
+        return subnetOctet;
+    }
+
+}
