7a888d486ea4339a254b7cbb0e7f960abba56d7f
Hovhannes Grigoryan <grhovo199811@gmail.com>
2021-04-14 10:53:55 +0400

    Second Tokosavorum

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // ip address definition in array
         int[] ipaddressoctets = new int[4];
+        // mask in integer
         int mask;
         Scanner scanner = new Scanner(System.in);
+        // define variables
         for(int i = 0; i < 4; i++) {
             System.out.println("Give ip address of octet " + (i + 1));
             ipaddressoctets[i] = scanner.nextInt();
@@ -15,18 +18,42 @@ public class Main {
         }
         System.out.println("Give subnet mask");
         mask = scanner.nextInt();
+        //check is the number is correct
         while (mask > 32 || mask < 0){
             System.out.println("Please give correct number for subnet mask");
             mask = scanner.nextInt();
         }
+        //Subnet calculator class definition
         subnetCalculator calculator = new subnetCalculator(ipaddressoctets,mask);
+        //Integer mask to x.x.x.x form, where x is integer
         int[] subnetOctet = calculator.convertMaskToOctets();
         for (int i = 0; i < 4; i++) {
             System.out.println(subnetOctet[i]);
         }
+        //Class definition
         ipClass classNetwork = calculator.classDefinition();
         System.out.println("Class for this ip: " + classNetwork.getClassWord());
         System.out.println(classNetwork.print());
+        int[] networkIP = calculator.networkIP(subnetOctet);
+        for (int i = 0; i < 4; i++) {
+            System.out.print(networkIP[i] + ".");
+        }
+        System.out.println();
+        int[] firstIP = calculator.firstIP(networkIP);
+        for (int i = 0; i < 4; i++) {
+            System.out.print(firstIP[i] + ".");
+        }
+        System.out.println();
+        int[] broadcastIP = calculator.broadcastIP(networkIP);
+        for (int i = 0; i < 4; i++) {
+            System.out.print(broadcastIP[i] + ".");
+        }
+        System.out.println();
+        int[] lastIP = calculator.lastIP(broadcastIP);
+        for (int i = 0; i < 4; i++) {
+            System.out.print(lastIP[i] + ".");
+        }
+        System.out.println();
     }
 
 }
--- src/subnetCalculator.java
+++ src/subnetCalculator.java
@@ -10,6 +10,7 @@ public class subnetCalculator {
     public int[] getIpaddressoctets() {
         return ipaddressoctets;
     }
+    // define class(A,B,C,D,E)
     public ipClass classDefinition() {
         ipClass classNetwork;
         if(ipaddressoctets[0] < 128){
@@ -29,7 +30,7 @@ public class subnetCalculator {
         }
         return classNetwork;
     }
-
+    //Convert Integer mask to x.x.x.x form, where x is integer
     public int[] convertMaskToOctets(){
         int maskChangedOctets = mask % 8;
         int fullQualifiedOctets = mask/8;
@@ -63,5 +64,62 @@ public class subnetCalculator {
     
     public int getMask() {
         return mask;
+    }
+    // Get Network Address
+    public int[] networkIP(int[] maskOctet){
+        int[] network = new int[4];
+        for (int i = 0; i < 4; i++) {
+            network[i] = ipaddressoctets[i] & maskOctet[i];
+        }
+        return network;
+    }
+    // Get Broadcast Address
+    public int[] broadcastIP(int networkIP[]){
+        int[] broadcast = new int[4];
+        if(mask<=8){
+            broadcast[0] = networkIP[0] + ((int) Math.pow(2,8-mask) )- 1;
+            broadcast[1] = 255;
+            broadcast[2] = 255;
+            broadcast[3] = 255;
+        }
+        else if(mask <= 16){
+            broadcast[0] = networkIP[0];
+            broadcast[1] = networkIP[1] + ((int) Math.pow(2,16-mask) )- 1;
+            System.out.println(broadcast[1]);
+            System.out.println(mask);
+            broadcast[2] = 255;
+            broadcast[3] = 255;
+        }
+        else if(mask <= 24){
+            broadcast[0] = networkIP[0];
+            broadcast[1] = networkIP[1];
+            broadcast[2] = networkIP[2] + ((int) Math.pow(2,24-mask) )- 1;
+            broadcast[3] = 255;
+        }
+        else {
+            broadcast[0] = networkIP[0];
+            broadcast[1] = networkIP[1];
+            broadcast[2] = networkIP[2];
+            broadcast[3] = networkIP[3] + ((int) Math.pow(2,32-mask) )- 1;
+        }
+        return broadcast;
+    }
+    // It's for first IP, just add 1 to last octet of ip address
+    public int[] firstIP(int networkIP[]){
+        int[] first = new int[4];
+        first[0] = networkIP[0];
+        first[1] = networkIP[1];
+        first[2] = networkIP[2];
+        first[3] = networkIP[3]+1;
+        return first;
+    }
+    //Find last IP address
+    public int[] lastIP(int broadcastIP[]){
+        int[] last = new int[4];
+        last[0] = broadcastIP[0];
+        last[1] = broadcastIP[1];
+        last[2] = broadcastIP[2];
+        last[3] = broadcastIP[3]-1;
+        return last;
     }
 }
